b8b988a4d810f0d5dd003eff93d03c9809592447
Jose Salvador Atemiz Martinez <jose.atemiz@tutum.com.mx>
2022-05-20 18:57:38 -0500

    ACTUALIZACIÓN DE PAGINA WEB TUTUM - TODAS LAS SECCIONES HATA EL MOMENTO

--- php/WEBSITE-TUTUM/resources/servicios/index.php
+++ php/WEBSITE-TUTUM/resources/servicios/index.php
@@ -25,50 +25,50 @@
 
 	    <li id="proyectos-active" class="service-menu-color" onclick="window.location.href='servicios-proyectos';">
 	    	<img class="fondos fondo-proyectos" src="<?php echo $url ?>images/img/serv.jpg">
-	      	<img src="<?php echo $url ?>images/icons/1.- ordenador-portatil.gif" class="icon-menu-black">
+	      	<img src="<?php echo $url ?>images/icons/services/1.- ordenador-portatil.gif" class="icon-menu-black">
 	      	<img src="<?php echo $url ?>images/icons/laptop-2.png" class="icon-menu-white" >
 	      	<h2>DESARROLLO DE PORYECTOS TI</h2>
 	    </li>
 
 	    <li id="proyectos-active" class="service-menu-color" onclick="window.location.href='servicios-operacion';">
 	    	<img class="fondos fondo-proyectos" src="<?php echo $url ?>images/img/operacion.jpg">
-	      	<img src="<?php echo $url;?>images/icons/2.- red-en-la-nube.gif" class="icon-menu-black">
-	      	<img src="<?php echo $url;?>images/icons/sap-2.png" class="icon-menu-white">
+	      	<img src="<?php echo $url ?>images/icons/services/2.- apreton-de-manos.gif" class="icon-menu-black">
+	      	<img src="<?php echo $url;?>images/icons/services/handshake.png" class="icon-menu-white">
 	      	<h2>OPERACIÓN DE PROYECTOS​</h2>
 	    </li>
 
 	    <li id="proyectos-active" class="service-menu-color" onclick="window.location.href='servicios-cau';">
 	    	<img class="fondos fondo-proyectos" src="<?php echo $url ?>images/img/FOTO 10.jpg">
-	      	<img src="<?php echo $url;?>images/icons/cau.png" class="icon-menu-black" >
-	      	<img src="<?php echo $url;?>images/icons/cau-2.png" class="icon-menu-white" >
+	      	<img src="<?php echo $url ?>images/icons/services/3.- apoyo-tecnico.gif" class="icon-menu-black">
+	      	<img src="<?php echo $url;?>images/icons/services/tech-support.png" class="icon-menu-white" >
 	      	<h2>CENTRO DE ATENCIÓN A USUARIOS​</h2>
 	    </li>
 
 	    <li id="proyectos-active" class="service-menu-color" onclick="window.location.href='servicios-sap';" >
 	    	<img class="fondos fondo-proyectos" src="<?php echo $url ?>images/img/FOTO 11.jpg">
-	      	<img src="<?php echo $url;?>images/icons/document-2.png" class="icon-menu-black" >
-	      	<img src="<?php echo $url;?>images/icons/document.png" class="icon-menu-white" >
+	      	<img src="<?php echo $url ?>images/icons/services/4.- presentacion.gif" class="icon-menu-black">
+	      	<img src="<?php echo $url;?>images/icons/services/presentation.png" class="icon-menu-white" >
 	      	<h2>CONSULTORÍA E IMPLEMENTACIÓN SAP​</h2>
 	    </li>
 
 	    <li id="proyectos-active" class="service-menu-color" onclick="window.location.href='servicios-hospitales';" >
 	    	<img class="fondos fondo-proyectos" src="<?php echo $url ?>images/img/FOTO 15.jpg">
-	      	<img src="<?php echo $url;?>images/icons/informe-medico-2.png" class="icon-menu-black" >
-	      	<img src="<?php echo $url;?>images/icons/informe-medico.png" class="icon-menu-white" >
+	      	<img src="<?php echo $url ?>images/icons/services/5.- medicamento.gif" class="icon-menu-black">
+	      	<img src="<?php echo $url;?>images/icons/services/medicine.png" class="icon-menu-white" >
 	      	<h2>SOLUCIONES HOSPITALARIAS​</h2>
 	    </li>
 
 	    <li id="proyectos-active" class="service-menu-color" onclick="window.location.href='servicios-ciberseguridad';" >
 	    	<img class="fondos fondo-proyectos" src="<?php echo $url ?>images/img/FOTO 06.jpg">
-	      	<img src="<?php echo $url;?>images/icons/encrypted-2.png" class="icon-menu-black" >
-	      	<img src="<?php echo $url;?>images/icons/encrypted.png" class="icon-menu-white" >
+	      	<img src="<?php echo $url ?>images/icons/services/6.- hacker.gif" class="icon-menu-black">
+	      	<img src="<?php echo $url;?>images/icons/services/hacker.png" class="icon-menu-white" >
 	      	<h2>CIBERSEGURIDAD<br><br></h2>
 	    </li>
 
 	    <li id="proyectos-active" class="service-menu-color" onclick="window.location.href='servicios-fabrica-sw';">
 	    	<img class="fondos fondo-proyectos" src="<?php echo $url ?>images/img/FOTO 10.jpg">
-	      	<img src="<?php echo $url;?>images/icons/engineering-2.png" class="icon-menu-black">
-	      	<img src="<?php echo $url;?>images/icons/engineering.png" class="icon-menu-white">
+	      	<img src="<?php echo $url ?>images/icons/services/7.- idea.gif" class="icon-menu-black">
+	      	<img src="<?php echo $url;?>images/icons/services/idea.png" class="icon-menu-white">
 	      	<h2>FABRICA DE SOFTWARE​</h2>
 	    </li>
 
