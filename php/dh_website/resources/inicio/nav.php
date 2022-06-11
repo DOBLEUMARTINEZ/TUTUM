8dae76175dcd5a40ca00a2108a4c234ebcd4939f
Jose Salvador Atemiz Martinez <jose.atemiz@tutum.com.mx>
2022-06-10 19:11:37 -0500

    ACTUALIZACIÓN DE PAGINA WEB TUTUM | RESPOSIVE - FORMULARIOS

--- php/dh_website/resources/inicio/nav.php
+++ php/dh_website/resources/inicio/nav.php
@@ -84,7 +84,7 @@
         <li style="width: 60%" >
           <ul >
             <li onclick="window.open('https://www.facebook.com/Tutum-Tech-515010355546588');" ><i class="fab fa-facebook-square"></i></li>
-            <li onclick="window.open('https://www.instagram.com/tutumtech/');" ><i class="fab fa-instagram"></i></li>
+            <li onclick="window.open('https://www.instagram.com/tutum.tech/');" ><i class="fab fa-instagram"></i></li>
             <li onclick="window.open('https://www.linkedin.com/company/tutum-tech/mycompany/verification/');" ><i class="fab fa-linkedin"></i></li>
             <li onclick="window.open('https://twitter.com/TutumTech');" ><i class="fab fa-twitter-square"></i></li>
           </ul>
