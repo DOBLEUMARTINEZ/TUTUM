f2ef7738d167cfd26d84d77ac4dbcc4aeafd2318
TutumHis <jose.atemiz@tutum.com.mx>
2022-12-22 19:03:58 -0600

    acualización dobleu martinez projects - kadcreativos.com

---
+++ php/SANET - COMPRES - 22122022/resources/secciones/sanet.php
@@ -0,0 +1,293 @@
+<!-- SANET  -->
+<div id="sanet-content">
+
+ <div class="bloque-1">
+
+    <div>
+
+      <p style="opacity:0;position: relative;padding: 0;margin: 0;">
+        es una solución integral, diseñada para administrar todas las áreas que integran a un hospital o clínica, centrada en la seguridad del paciente por medio de la optimización de los procesos asistenciales y colaborando en la reducción de costos de la atención en salud.<br><br>
+        El objetivo de <span>sanet</span> es implementar el Sistema de Información Hospitalario (SIH), mediante el cual se logra una comunicación permanente entre todas las áreas hospitalarias. Por medio del SIH el personal médico y administrativo logrará tener una gestión oportuna, clara y efectiva de sus actividades, coadyuvando al logro de los indicadores de productividad y estándares de calidad en los servicios.<br><br>
+      </p>
+
+      <p>
+        <a></a>es una solución integral, diseñada para administrar todas las áreas que integran a un hospital o clínica, centrada en la seguridad del paciente por medio de la optimización de los procesos asistenciales y colaborando en la reducción de costos de la atención en salud.<br><br>
+        El objetivo de <span>sanet</span> es implementar el Sistema de Información Hospitalario (SIH), mediante el cual se logra una comunicación permanente entre todas las áreas hospitalarias. Por medio del SIH el personal médico y administrativo logrará tener una gestión oportuna, clara y efectiva de sus actividades, coadyuvando al logro de los indicadores de productividad y estándares de calidad en los servicios.<br><br>
+      </p>
+
+    </div>
+    
+    <img src="images/img/modulos-sih-1.jpg"  class="desktop" title="sanet" alt="Doctores en Quirófano en Cirugía utilizando Tablet con Sistema Hospitalario sanet">
+    <img src="images/img/banner-sanet-3.jpg"  class="tablet" title="sanet" alt="Doctores en Quirófano en Cirugía utilizando Tablet con Sistema Hospitalario sanet">
+    <img src="images/img/banner-sanet-2.jpg" class="mobil" title="sanet" alt="Doctores en Quirófano en Cirugía utilizando Tablet con Sistema Hospitalario sanet">
+
+ </div>
+
+</div>
+
+<br><br>
+
+<div id="modulo-index-content">
+
+  <div class="bloque-2" >
+    
+    <ul>
+      <li>
+        <span><img src="images/icons/modulos sih/investigacion.png" title="Módulos administrativos" alt="icono adminstrativo de color"></span>
+        <h2>Módulos administrativos</h2>
+        <p>
+          <span>sanet</span> cuenta con todos los módulos necesarios para la gestión administrativa de tu hospital, incluyendo la gestión de almacenes, proveedores, cuentas de pacientes y facturación.
+        </p>
+        <button onclick="window.location.href='./modulos-sih#modulos-administrativos';">ver más detalles <i class="fas fa-angle-double-right"></i></button>
+      </li>
+      <li>
+        <span><img src="images/icons/modulos sih/medical-record.png" title="Módulos clínicos" alt="icono reporte clínico de color"></span>
+        <h2>Módulos clínicos</h2>
+        <p>
+          <span>sanet</span> cuenta con módulos de atención clínica para todas las áreas hospitalarias, teniendo siempre en el centro de atención al paciente.
+        </p>
+        <button onclick="window.location.href='./modulos-sih#modulos-clinicos';">ver más detalles <i class="fas fa-angle-double-right"></i></button>
+      </li>
+      <li>
+        <span><img src="images/icons/modulos sih/settings.png" title="Módulos de configuración" alt="icono de configuración clínica de color"></span>
+        <h2>Módulos de configuración</h2>
+        <p>
+          <span>sanet</span> cuenta con módulos para la configuración de diversos aspectos del hospital, permitiendo un alto grado de adaptación de acuerdo a sus necesidades.
+        </p>
+        <button onclick="window.location.href='./modulos-sih#modulos-configuracion';" >ver más detalles <i class="fas fa-angle-double-right"></i></button>
+      </li>
+    </ul>
+
+  </div>
+
+</div>
+
+<div id="modulo-sih-content">
+
+  <div class="bloque-3" style="display: none;">
+    <div>
+      <h2>Expediente clínico electrónico</h2>
+      <p>El expediente electrónico es la columna vertebral de la información del paciente, ya que contiene todos los eventos del paciente como son consultas médicas, urgencias, estudios ambulatorios e internamiento en hospital para tratamientos y/o cirugías.</p>
+    </div>
+    <video class="video" loading="lazy" src="images/HOSPITAL.mp4" autoplay="" muted="" loop=""></video>
+  </div>
+
+  <div class="bloque-4 desktop">
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+
+      <div class="carousel-inner">
+
+        <div class="carousel-item content-slider active">
+          <div>
+            <ol>
+              <li>
+                <h2>Hospitalización</h2>
+                <p>
+                  El módulo de hospitalización de <span>sanet</span> comprende toda la atención a pacientes no ambulatorios. Comprende el ingreso del paciente a un piso de hospitalización, la gestión del tablero de camas y todas las actividades clínicas relacionadas a la atención del paciente, incluyendo las solicitudes de medicamentos y estudios auxiliares, interconsultas y transferencias entre servicios. Todas las especialidades médicas, incluyendo las áreas de terapia son incluidas en este módulo.
+                </p>
+              </li>
+            </ol>
+            <ul>
+              <li>
+                <ul>
+                  <li><h3>Funcionalidad</h3></li>
+                  <li>Admisión hospitalaria</li>
+                  <li>Tablero de camas<li>
+                  <li>Registro de historia clínica y antecedentes<li>
+                  <li>Solicitud de medicamentos<li>
+                  <li>Solicitud de estudios auxiliares de diagnóstico y tratamiento<li>
+                  <li>Esquema de insulinas<li>
+                  <li>Registro de formatos médicos y de enfermería<li>
+                  <li>Interconsultas y transferencia entre servicios.<li>
+                  <li>Registro y gráfica de signos vitales<li>
+                  <li>Administración y devolución de medicamentos<li>
+                  <li>Registro de procedimientos<li>
+                  <li>Proceso de alta médica<li>
+                  <li>Visualización e impresión de expediente clínico<li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <img src="images/img/modulos-sih-hospitalizacion-3.jpg" title="sanet" alt="Enfermera atendiendo a Paciente Hospitalizado en Camilla en Hospital con Sistema Hospitalario sanet">
+        </div>
+
+        <div class="carousel-item content-slider">
+          <div>
+            <ol>
+              <li>
+                <h2>Urgencias</h2>
+                <p>
+                  Para la adecuada atención del paciente en áreas de urgencia es vital la agilidad en los procesos. El módulo de urgencias de <span>sanet</span> toma en consideración esta premisa, permitiendo el registro del paciente de manera sencilla y ágil, incluso para pacientes desconocidos. El Triage es completamente configurable de acuerdo a las necesidades del hospital y la atención clínica comprende todo el ciclo de atención de los pacientes, hasta su egreso a domicilio, a quirófanos o su internamiento.
+                </p>
+              </li>
+            </ol>
+            <ul>
+              <li>
+                <ul>
+                  <li><h3>Funcionalidad</h3></li>
+                  <li>Registro rápido de paciente</li>
+                  <li>Triage</li>
+                  <li>Atención de primer contacto</li>
+                  <li>Atención en áreas de observación</li>
+                  <li>Interconsultas</li>
+                  <li>Registro de formatos médicos y de enfermería<li>
+                  <li>Solicitud de medicamentos</li>
+                  <li>Solicitud de estudios auxiliares de diagnóstico y tratamiento.</li>
+                  <li>Movimiento en camas y áreas de urgencias.</li>
+                  <li>Administración y devolución de medicamentos.</li>
+                  <li>Registro y gráfica de signos vitales</li>
+                  <li>Registro de procedimientos</li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <img src="images/img/modulos-sih-urgencias.jpg" title="Urgencias" alt="Dotores con Paciente en Urgencias en Hospital con Sistema Hospitalario sanet">
+        </div>
+
+        <div class="carousel-item content-slider">
+          <div>
+            <ol>
+              <li>
+                <h2>Consulta externa</h2>
+                <p>
+                  El módulo de consulta externa de <span>sanet</span> inicia con la administración y configuración de las agendas, ligándolas a los médicos y a las prestaciones disponibles en el hospital, indicando los tiempos de atención por cada uno. La gestión de las citas del paciente se realiza de una manera ágil y sencilla, registrando todos los tiempos involucrados en la cita para generar toda la estadística necesaria. El personal médico cuenta con todas las funcionalidades necesarias para la atención del paciente, incluyendo notas, estudios, recetas e interconsultas.
+                </p>
+              </li>
+            </ol>
+            <ul>
+              <li>
+                <ul>
+                  <li><h3>Funcionalidad</h3></li>
+                  <li>Configuración de agendas</li>
+                  <li>Configuración de prestaciones</li>
+                  <li>Agendamiento</li>
+                  <li>Registro de tiempos para estadística (llegada, atención, finalización de consulta)</li>
+                  <li>Visualización de agenda del día</li>
+                  <li>Atención de paciente</li>
+                  <li>Emisión de recetas</li>
+                  <li>Emisión de incapacidades</li>
+                  <li>Solicitud de estudios auxiliares</li>
+                  <li>Interconsultas</li>
+                  <li>Registro de notas médicas</li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <img src="images/img/modulos-sih-consulta-externa-2.jpg" title="Consulta externa" alt="Escritorio de Doctor con Laptop y cosas médicas">
+        </div>
+
+        <div class="carousel-item content-slider">
+          <div>
+            <ol>
+              <li>
+                <h2>Tococirugía</h2>
+                <p>
+                  El módulo de tococirugía de <span>sanet</span> le permitirá a todo el personal clínico involucrado en el proceso obstétrico llevar un control de toda la evolución del evento, registrando tanto datos de la madre como del producto y teniendo un seguimiento completo hasta la resolución del evento. Gracias a las funcionalidades que incluye el módulo se gestionan todo tipo de atenciones, incluyendo partos, cesáreas y abortos y el seguimiento al binomio hasta su eventual egreso de la institución.
+                </p>
+              </li>
+            </ol>
+            <ul>
+              <li>
+                <ul>
+                  <li><h3>Funcionalidad</h3></li>
+                  <li>Ingreso de paciente</li>
+                  <li>Triage obstétrico</li>
+                  <li>Atención en área tocoquirúrgica</li>
+                  <li>Proceso quirúrgico para cesárea</li>
+                  <li>Conducción de parto, partograma.</li>
+                  <li>Registro de signos vitales de madre y fetales</li>
+                  <li>Registro de evento obstétrico</li>
+                  <li>Sala de labor</li>
+                  <li>Sala de recuperación obstétrica</li>
+                  <li>Ingreso de binomio</li>
+                  <li>Elaboración de formatos médico y de enfermería</li>
+                  <li>Solicitud de medicamentos</li>
+                  <li>Solicitud de estudios auxiliares de diagnóstico y tratamiento.</li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <img src="images/img/modulos-sih-tococirugia-2.jpg" 
+         title="Tococirugía" alt="Doctores en Cuarto de Cirugía Atendiendo a Paciente Recostado en Camilla">
+        </div>
+
+        <div class="carousel-item content-slider">
+          <div>
+            <ol>
+              <li>
+                <h2>Quirófano</h2>
+                <p>
+                  El módulo de quirófanos de <span>sanet</span> comprende toda la administración de estas áreas en el hospital, ya sean quirófanos centrales, ambulatorios, de urgencias o de especialidades específicas. La agenda quirúrgica contempla toda la programación, incluyendo el equipamiento de CEyE necesario para cada intervención. El personal clínico cuenta con todos los formatos quirúrgicos necesarios para el registro de sus actividades y todos los tiempos quirúrgicos.
+                </p>
+              </li>
+            </ol>
+            <ul>
+              <li>
+                <ul>
+                  <li><h3>Funcionalidad</h3></li>
+                  <li>Configuración de quirófanos</li>  
+                  <li>Agendamiento de cirugías</li> 
+                  <li>Formatos quirúrgicos, anestésicos y de enfermería</li>  
+                  <li>Registro de tiempos quirúrgicos</li>  
+                  <li>Gestión de charolas de CEyE</li>  
+                  <li>Solicitud de medicamentos</li>  
+                  <li>Solicitud de estudios auxiliares de diagnóstico y tratamiento.</li> 
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <img src="images/img/modulos-sih-quirofano.jpg" title="Quirófano" alt="Quirófano Preparado para Cirugía con Cama de Quirófano en el Centro de Color azul">
+        </div>
+
+      </div>
+
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev" style="font-size: 6em;font-weight: bold;
+    color: #ffc107;">
+        <
+      </a>
+
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next" style="font-size: 6em;font-weight: bold;
+    color: #ffc107;">
+        >
+      </a>
+
+    </div>
+
+    <script type="text/javascript">
+      
+      $('.carousel').carousel({
+        interval: 1000
+      });
+
+    </script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+
+  </div>
+
+</div>
+
+<div id="modulo-index-content">
+
+  <div class="bloque-4">
+    <div>
+      <h2>Mejora la atención del paciente y resguarda de una manera segura los expedientes clínicos</h2>
+      <button onclick="window.location.href='./modulos-his';" >¡Quiero saber más!</button>
+    </div>
+  </div>
+
+</div>
+
+
+
+
+
+
+
+
+
