0f17aeb319c884d7cc9428807bc8ac7f12bea0b3
TutumHis <jose.atemiz@tutum.com.mx>
2023-01-17 16:22:14 -0600

    acualización

--- php/DOBLEUMARTINEZ/develop/PROJECTS/KADCREATIVOS/resources/sections/inicio/contenido.php
+++ php/DOBLEUMARTINEZ/develop/PROJECTS/KADCREATIVOS/resources/sections/inicio/contenido.php
@@ -21,33 +21,37 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-	  <div class="carousel-inner">
-	    <div class="carousel-item active">
-	    	<h2>CONTENIDO WEB</h2>
-	      <img class="d-block w-100" src="<?php echo $url; ?>/resources/images/banner-3.jpg" alt="First slide">
-	    </div>
-	    <div class="carousel-item">
-	    	<h2>BRANDING</h2>
-	      <img class="d-block w-100" src="<?php echo $url; ?>/resources/images/banner-4.jpg" alt="Second slide">
-	    </div>
-	    <div class="carousel-item">
-	    	<h2>REDES SOCIALES</h2>
-	      <img class="d-block w-100" src="<?php echo $url; ?>/resources/images/banner-5.jpg" alt="Third slide">
-	    </div>
-	  </div>
-	  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-	    <span class="sr-only">Previous</span>
-	  </a>
-	  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-	    <span class="sr-only">Next</span>
-	  </a>
+
+	  	<div class="carousel-inner">
+		    <div class="carousel-item active">
+		    	<h2>CONTENIDO WEB</h2>
+		      	<img class="d-block w-100" src="resources/images/banner-3.jpg" alt="First slide">
+		    </div>
+		    <div class="carousel-item">
+		    	<h2>BRANDING</h2>
+		      	<img class="d-block w-100" src="resources/images/banner-4.jpg" alt="Second slide">
+		    </div>
+		    <div class="carousel-item">
+		    	<h2>REDES SOCIALES</h2>
+		      	<img class="d-block w-100" src="resources/images/banner-5.jpg" alt="Third slide">
+		    </div>
+	  	</div>
+
+	  	<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+	    	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    	<span class="sr-only">Previous</span>
+	  	</a>
+
+	  	<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+	    	<span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    	<span class="sr-only">Next</span>
+	  	</a>
+
 	</div>
 
     <!-- Bootstrap core JavaScript -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-    
+
 </div>
 
 <div class="box-services-2" >
