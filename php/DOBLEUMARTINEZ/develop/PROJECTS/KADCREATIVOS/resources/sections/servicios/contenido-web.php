9f48cc2a78222c88bfbce1aa458caf40fa4a9d17
TutumHis <jose.atemiz@tutum.com.mx>
2023-03-15 18:55:37 -0600

    acualización

--- php/DOBLEUMARTINEZ/develop/PROJECTS/KADCREATIVOS/resources/sections/servicios/contenido-web.php
+++ php/DOBLEUMARTINEZ/develop/PROJECTS/KADCREATIVOS/resources/sections/servicios/contenido-web.php
@@ -1,8 +1,8 @@
-<!-- BANNER-1 -->
-<div class="box-slider">
+<!-- Bootstrap core CSS -->
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Bootstrap core CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+<!-- BANNER -->
+<div class="box-slider">
 
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" style="box-shadow: 0px 0px 10px 2px #7f3f98ab;">
 
@@ -22,17 +22,63 @@
 
 	</div>
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-
 </div>
 
-<div class="box-services-2">
+<!-- BOX SERVICES -->
+<div class="box-services-second">
 	<h2>
-		Personalizamos e imprimimos sobre vinil, con o sin suaje, color solido o transparente.
+		¿CUÁL ES EL PAQUETE IDEAL PARA MÍ?
 	</h2>
+	<div>
+		<div>
+			<h3>Paquetes Wordpress</h3>
+			<ol>
+				<li>Fácil gestión</li>
+				<li>Aún sin experiencia en código, podrás editar fácilmente imágenes o textos</li>
+				<li>Uso de plantillas</li>
+				<li>Se pueden integrar plugins gratuitos o de pago para mejorar la funcionalidad del sitio</li>
+			</ol>
+			<button class="cta-1">Contactanos via Whatsapp</button>
+		</div>
+		<div>
+			<div id="carouselExampleControls-2" class="carousel slide" data-ride="carousel">
+
+			  	<div class="carousel-inner">
+
+				    <div class="carousel-item active">
+				      	<img class="d-block w-100" src="./resources/images/stickers-1.jpg" title="Contenido web" alt="Contenido web" onclick="window.location.href='<?php echo $url?>contenido-web';">
+				    </div>
+
+				    <div class="carousel-item">
+				      	<img class="d-block w-100" src="./resources/images/stickers-2.jpg" title="Foto y video" alt="Foto y video" onclick="window.location.href='<?php echo $url?>foto-y-video';">
+				    </div>
+
+				    <div class="carousel-item">
+				      	<img class="d-block w-100" src="./resources/images/stickers-3.jpg" title="Imagen corporiativa" alt="Imagen corporiativa" onclick="window.location.href='<?php echo $url?>imagen-corporativa';">
+				    </div>
+
+				    <div class="carousel-item">
+				      	<img class="d-block w-100" src="./resources/images/stickers-4.jpg" title="Impresion" alt="Impresion" onclick="window.location.href='<?php echo $url?>impresion';">
+				    </div>
+
+			  	</div>
+
+			  	<a class="carousel-control-prev" href="#carouselExampleControls-2" role="button" data-slide="prev">
+			    	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    	<span class="sr-only">Previous</span>
+			  	</a>
+
+			  	<a class="carousel-control-next" href="#carouselExampleControls-2" role="button" data-slide="next">
+			    	<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			    	<span class="sr-only">Next</span>
+			  	</a>
+
+			</div>
+		</div>
+	</div>
 </div>
 
+<!-- CONTENIDO WEB 
 <div class="box-services-2" >
 	<div>
 		<h3>Paquetes Wordpress</h3>
@@ -40,7 +86,7 @@
 			<li>fdgdf</li>
 			<li>fdgdf</li>
 		</ul>
-		<button class="cta-1">Contactanos via Whatsapp</button>
+		
 	</div>
 	<div style="display: flex;justify-content: center;">
 		<ol class="gallery-wm">
@@ -50,7 +96,7 @@
 			<li class="img-4" style="background-image: url('./resources/images/stickers-4.jpg');"><span>4</span></li>
 		</ol>
 	</div>
-</div>
+</div>-->
 
 <hr>
 
@@ -64,3 +110,6 @@
         <button onclick="window.location.href='<?php echo $url?>redes-sociales';">Redes sociales</button><br>
 	</div>
 </div>
+
+<!-- Bootstrap core JavaScript -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
